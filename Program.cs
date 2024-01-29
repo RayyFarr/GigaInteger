f51f0bc2a63cd184dbc9c888dc998b303fdd2e4c
RayyFarr <dohvakiin555@yahoo.com>
2024-01-30 00:15:52 +0600

    Added a digits array and started working on multiplication. May change project structure later.

--- Program.cs
+++ Program.cs
@@ -24,11 +24,34 @@ namespace GigaInteger
     public class GigaInt
     {
         public readonly string value;
+        public readonly int[] digits;
 
         #region Constructors
-        private GigaInt(string value) => this.value = value;
-        private GigaInt(int value) => this.value = value.ToString();
-        private GigaInt(long value) => this.value = value.ToString();
+        private GigaInt(string value)
+        {
+            this.value = value;
+            digits = StringToDigits(value);
+        }
+        private GigaInt(int value)
+        {
+            this.value = value.ToString();
+            digits = StringToDigits(value.ToString());
+        }
+        private GigaInt(long value)
+        {         
+            this.value = value.ToString();
+            digits = StringToDigits(value.ToString());
+        }
+
+        public int[] StringToDigits(string value)
+        {
+            int[] digits = new int[value.Length];
+            for (int i = 0; i < value.Length; i++)          
+                digits[i] = int.Parse(value[i].ToString());
+
+            return digits;
+        }
+
         #endregion
 
 
@@ -36,6 +59,8 @@ namespace GigaInteger
         public static implicit operator GigaInt(int value) => new GigaInt(value);
         public static implicit operator GigaInt(long value) => new GigaInt(value);
         public static implicit operator GigaInt(string value) => new GigaInt(value);
+
+
         #endregion
 
 
@@ -44,16 +69,25 @@ namespace GigaInteger
         //Will implement the operators later. These are placeholders
         public static GigaInt operator +(GigaInt a, GigaInt b) => a.value + b.value;
         public static GigaInt operator -(GigaInt a, GigaInt b) => a.value + b.value;
-        public static GigaInt operator *(GigaInt a, GigaInt b) => a.value + b.value;
+        public static GigaInt operator *(GigaInt a, GigaInt b)
+        {
+            int[] digitsA = a.digits;
+            int[] digitsB = b.digits;
+
+            int[] multiplied = a.digits.Length > b.digits.Length ? a.digits : b.digits;
+            for (int i = 0; i < Math.Min(digitsA.Length, digitsB.Length); i++)
+            {
+
+            }
+        }
         public static GigaInt operator /(GigaInt a, GigaInt b) => a.value + b.value;
         public static GigaInt operator ^(GigaInt a, GigaInt b) => a.value + b.value;
 
         #endregion
 
-
+        
         #region Misc
         public override string ToString() => value;
-
         #endregion
     }
 }
