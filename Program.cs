44fd5044c3abdd475a5bcdefb6592003b7f41bd6
RayyFarr <dohvakiin555@yahoo.com>
2024-02-02 23:50:06 +0600

    Started working on addition
    Removed Multiply changes and started working on addition

--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ namespace GigaInteger
 {
     internal class Program
     {
-        static GigaInt num1 = 123;
-        static GigaInt num2 = "321";
-        static GigaInt num3 = "230124856092837675328479773473675489679234672572439031023712763123";
 
         static void Main(string[] args)
         {
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
-            Console.WriteLine(num3);
+            Console.WriteLine("Write the first input: ");
+            GigaInt n1 = Console.ReadLine();
+
+            Console.WriteLine("Write the second input: ");    
+            GigaInt n2 = Console.ReadLine();
+
+            Console.WriteLine("Sum: " + (n1 + n2).ToString());
 
         }
     }
@@ -43,8 +44,16 @@ namespace GigaInteger
             digits = StringToDigits(value.ToString());
         }
 
+        private GigaInt(int[] digits)
+        {
+            this.digits = digits;
+            string v = "";
+            foreach (int digit in digits) v += digit.ToString();
+        }
+
         public int[] StringToDigits(string value)
         {
+
             int[] digits = new int[value.Length];
             for (int i = 0; i < value.Length; i++)          
                 digits[i] = int.Parse(value[i].ToString());
@@ -67,25 +76,30 @@ namespace GigaInteger
         #region Operations
 
         //Will implement the operators later. These are placeholders
-        public static GigaInt operator +(GigaInt a, GigaInt b) => a.value + b.value;
-        public static GigaInt operator -(GigaInt a, GigaInt b) => a.value + b.value;
-        public static GigaInt operator *(GigaInt a, GigaInt b)
+        public static GigaInt operator +(GigaInt a, GigaInt b)
         {
-            int[] digitsA = a.digits;
-            int[] digitsB = b.digits;
+            string result = "";
 
-            int[] multiplied = a.digits.Length > b.digits.Length ? a.digits : b.digits;
-            for (int i = 0; i < Math.Min(digitsA.Length, digitsB.Length); i++)
+            bool carry = false;
+
+            for(int i = Math.Max(a.digits.Length,b.digits.Length)-1;i>=0;i--)
             {
-
+                int r = (i<a.digits.Length?a.digits[i]:0) + (i<b.digits.Length?b.digits[i]:0) + (carry?1:0);
+                result = r%10==0?"0":(r % 10).ToString() + result;
+                if (r >= 10)
+                {
+                    carry = true;
+                }
             }
+
+            return new GigaInt((carry?"1":"") + result);
         }
+        public static GigaInt operator -(GigaInt a, GigaInt b) => a.value + b.value;
+        public static GigaInt operator *(GigaInt a, GigaInt b) => a.value + b.value;
         public static GigaInt operator /(GigaInt a, GigaInt b) => a.value + b.value;
         public static GigaInt operator ^(GigaInt a, GigaInt b) => a.value + b.value;
 
         #endregion
-
-        
         #region Misc
         public override string ToString() => value;
         #endregion
